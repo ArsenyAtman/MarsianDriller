311d2129b3de5e9c0b876fc106c730048f1a7af6
ArsenyAtman <arsenmail2018@gmail.com>
2022-10-24 18:54:58 +0300

    Initial commit

---
+++ Source/MarsianDriller/MarsianDriller.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MarsianDriller.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MarsianDriller, "MarsianDriller" );
